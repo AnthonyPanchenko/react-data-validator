3c340d7f210613dd52e43aace79a59ec228aae72
Anton Panchenko <nixegend@gmail.com>
2023-07-07 16:21:26 +0300

    clone element

--- src/drag-and-drop-sorting/DragAndDropSortingSource.tsx
+++ src/drag-and-drop-sorting/DragAndDropSortingSource.tsx
@@ -1,6 +1,6 @@
 import './drag-and-drop-sorting.scss';
 
-import { useCallback, useRef, useState } from 'react';
+import { forwardRef, Fragment, Ref, useCallback, useRef, useState } from 'react';
 
 type PropsTypes = {
   index: number;
@@ -32,7 +32,7 @@ export default function DragAndDropSortingSource({
     if (typeof onRegisterDragItem === 'function' && elementRef.current) {
       const elementClientRect = elementRef.current.getBoundingClientRect();
       elementWidth.current = elementClientRect.width;
-      console.log(elementClientRect);
+      // console.log(elementClientRect);
       onRegisterDragItem(
         setPosition,
         setActiveState,
@@ -50,27 +50,51 @@ export default function DragAndDropSortingSource({
     currentClassName += ' ' + className;
   }
 
-  if (isActive) {
-    currentClassName += ' ' + 'active';
-  }
-
   return (
-    <div
-      ref={elementRef}
-      style={
-        {
-          '--cord-y': position + 'px',
-          '--width': elementWidth.current + 'px'
-        } as React.CSSProperties
-      }
-      className={currentClassName}
-    >
-      <span role="button" className="drag-handler" tabIndex={0} onMouseDown={onMouseDown}>
-        <svg viewBox="0 0 11 17" xmlns="http://www.w3.org/2000/svg">
-          <path d="M8.5 12.484c.532 0 1 .203 1.406.609.406.406.609.875.609 1.406 0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609-.531 0-1-.203-1.406-.609-.406-.406-.609-.874-.609-1.406 0-.531.203-1 .609-1.406.406-.406.875-.609 1.406-.609Zm0-6c.532 0 1 .203 1.406.609.406.406.609.875.609 1.406 0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609-.531 0-1-.203-1.406-.609-.406-.406-.609-.874-.609-1.406 0-.531.203-1 .609-1.406.406-.406.875-.609 1.406-.609Zm0-1.968c-.531 0-1-.203-1.406-.609-.406-.406-.609-.874-.609-1.406 0-.531.203-1 .609-1.406C7.5.689 7.969.486 8.5.486c.532 0 1 .203 1.406.609.406.406.609.875.609 1.406 0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609ZM2.5.484c.532 0 1 .203 1.406.609.406.406.609.875.609 1.406 0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609-.531 0-1-.203-1.406-.609-.406-.406-.609-.874-.609-1.406 0-.531.203-1 .609-1.406C1.5.687 1.969.484 2.5.484Zm0 6c.532 0 1 .203 1.406.609.406.406.609.875.609 1.406 0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609-.531 0-1-.203-1.406-.609-.406-.406-.609-.874-.609-1.406 0-.531.203-1 .609-1.406.406-.406.875-.609 1.406-.609ZM4.516 14.5c0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609-.531 0-1-.203-1.406-.609C.689 15.5.486 15.032.486 14.5c0-.531.203-1 .609-1.406.406-.406.875-.609 1.406-.609.532 0 1 .203 1.406.609.406.406.609.875.609 1.406Z" />
-        </svg>
-      </span>
-      {children}
-    </div>
+    <Fragment>
+      {isActive && (
+        <DragAndDropBaseSource
+          styles={
+            {
+              '--cord-y': position + 'px',
+              '--width': elementWidth.current + 'px'
+            } as React.CSSProperties
+          }
+          className={currentClassName + ' active'}
+        >
+          {children}
+        </DragAndDropBaseSource>
+      )}
+      <DragAndDropBaseSource
+        ref={elementRef}
+        className={isActive ? currentClassName + ' inactive' : currentClassName}
+        onMouseDown={onMouseDown}
+      >
+        {children}
+      </DragAndDropBaseSource>
+    </Fragment>
   );
 }
+
+type DnDBaseSourcePropsTypes = {
+  className?: string;
+  children?: React.ReactNode | React.ReactNode[] | null;
+  styles?: React.CSSProperties;
+  onMouseDown?: (event: React.MouseEvent<HTMLDivElement>) => void;
+};
+
+const DragAndDropBaseSourceContainer = (
+  { children, className, styles, onMouseDown }: DnDBaseSourcePropsTypes,
+  ref?: Ref<HTMLDivElement>
+) => (
+  <div ref={ref} style={styles} className={className}>
+    <span role="button" className="drag-handler" tabIndex={0} onMouseDown={onMouseDown}>
+      <svg viewBox="0 0 11 17" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.5 12.484c.532 0 1 .203 1.406.609.406.406.609.875.609 1.406 0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609-.531 0-1-.203-1.406-.609-.406-.406-.609-.874-.609-1.406 0-.531.203-1 .609-1.406.406-.406.875-.609 1.406-.609Zm0-6c.532 0 1 .203 1.406.609.406.406.609.875.609 1.406 0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609-.531 0-1-.203-1.406-.609-.406-.406-.609-.874-.609-1.406 0-.531.203-1 .609-1.406.406-.406.875-.609 1.406-.609Zm0-1.968c-.531 0-1-.203-1.406-.609-.406-.406-.609-.874-.609-1.406 0-.531.203-1 .609-1.406C7.5.689 7.969.486 8.5.486c.532 0 1 .203 1.406.609.406.406.609.875.609 1.406 0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609ZM2.5.484c.532 0 1 .203 1.406.609.406.406.609.875.609 1.406 0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609-.531 0-1-.203-1.406-.609-.406-.406-.609-.874-.609-1.406 0-.531.203-1 .609-1.406C1.5.687 1.969.484 2.5.484Zm0 6c.532 0 1 .203 1.406.609.406.406.609.875.609 1.406 0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609-.531 0-1-.203-1.406-.609-.406-.406-.609-.874-.609-1.406 0-.531.203-1 .609-1.406.406-.406.875-.609 1.406-.609ZM4.516 14.5c0 .532-.203 1-.609 1.406-.406.406-.874.609-1.406.609-.531 0-1-.203-1.406-.609C.689 15.5.486 15.032.486 14.5c0-.531.203-1 .609-1.406.406-.406.875-.609 1.406-.609.532 0 1 .203 1.406.609.406.406.609.875.609 1.406Z" />
+      </svg>
+    </span>
+    {children}
+  </div>
+);
+
+const DragAndDropBaseSource = forwardRef(DragAndDropBaseSourceContainer);
